c84ed5cc95d646fd0a9954b052db0a46c3456f94
Éverton M. Vieira <everton.muvi@gmail.com>
2022-05-08 17:51:34 -0300

    prototype development

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
-#include <iostream>
 #include <chrono>
+#include <iostream>
 #include <thread>
 
 using namespace std;
@@ -8,12 +8,16 @@ int main() {
   cout << "LongWrite starting...\n";
   cout << "We will write one line each half second for three minutes...\n";
   FILE *file;
-  file = fopen("longwrite.txt", "w");
+  errno_t error = fopen_s(&file, "longwrite.txt", "w");
+  if (error > 0) {
+    cout << "Could not open the file.\n";
+    return 1;
+  }
   for (int i = 0; i < 360; ++i) {
-	  fprintf(file, "This is a line.\n");
-	  fflush(file);
-	  cout << "Wrote one line.\n";
-	  this_thread::sleep_for(chrono::milliseconds(500));
+    fprintf(file, "This is a line.\n");
+    fflush(file);
+    cout << "Wrote one line.\n";
+    this_thread::sleep_for(chrono::milliseconds(500));
   }
   fclose(file);
 }
