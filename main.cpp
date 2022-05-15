5f4b96ea5fc19ffff7177a5c8a5f235eb08beaca
Éverton M. Vieira <emuvi@goutlook.com.br>
2022-05-15 07:06:28 -0300

    organizes and makes it possible to print to stderr

--- main.cpp
+++ main.cpp
@@ -1,47 +1,63 @@
 #include <chrono>
+#include <corecrt_wstdio.h>
 #include <iostream>
+#include <stdio.h>
 #include <thread>
 
-bool is_param_of(char *param, char const *short_kind, char const *long_kind) {
-  return (strcmp(param, short_kind) == 0 || strcmp(param, long_kind) == 0);
+int times = 360;
+int sleep = 500;
+char *file = NULL;
+char *line = (char *)"This is a line!";
+FILE *destiny = stdout;
+
+bool check(char *param, char const *is_short, char const *or_long) {
+  return (strcmp(param, is_short) == 0 || strcmp(param, or_long) == 0);
+}
+
+void parse(int argc, char *argv[]) {
+  for (int i = 1; i < argc; i++) {
+    if (check(argv[i], "-e", "---stderr")) {
+      destiny = stderr;
+    } else if (check(argv[i], "-t", "--times")) {
+      if (i < argc - 1) {
+        times = atoi(argv[i + 1]);
+        i++;
+      }
+    } else if (check(argv[i], "-s", "--sleep")) {
+      if (i < argc - 1) {
+        sleep = atoi(argv[i + 1]);
+        i++;
+      }
+    } else if (check(argv[i], "-f", "--file")) {
+      if (i < argc - 1) {
+        file = argv[i + 1];
+        i++;
+      }
+    } else if (check(argv[i], "-l", "--line")) {
+      if (i < argc - 1) {
+        line = argv[i + 1];
+        i++;
+      }
+    }
+  }
 }
 
 int main(int argc, char *argv[]) {
-  int times = 360;
-  int sleep = 500;
-  char *file_name = NULL;
-  char *line = (char *)"This is a line!";
-  for (int i = 1; i < argc - 1; i++) {
-    if (is_param_of(argv[i], "-t", "--times")) {
-      times = atoi(argv[i + 1]);
-    } else if (is_param_of(argv[i], "-s", "--sleep")) {
-      sleep = atoi(argv[i + 1]);
-    } else if (is_param_of(argv[i], "-f", "--file")) {
-      file_name = argv[i + 1];
-    } else if (is_param_of(argv[i], "-l", "--line")) {
-      line = argv[i + 1];
-    }
-  }
-  std::cout << "LongWrite starting...\n";
-  std::cout << "We will write one line " << times << " times at each " << sleep
-            << " miliseconds...\n";
-  if (file_name) {
-    FILE *file;
-    errno_t error = fopen_s(&file, file_name, "w");
+  parse(argc, argv);
+  if (file) {
+    errno_t error = fopen_s(&destiny, file, "w");
     if (error > 0) {
       std::cerr << "Could not open the file.\n";
       return 1;
     }
-    for (int i = 0; i < times; i++) {
-      fprintf(file, "%s\n", line);
-      fflush(file);
-      std::this_thread::sleep_for(std::chrono::milliseconds(sleep));
-    }
-    fclose(file);
-  } else {
-    for (int i = 0; i < times; i++) {
-      std::cout << line << std::endl;
-      std::this_thread::sleep_for(std::chrono::milliseconds(sleep));
-    }
   }
+  fprintf(destiny, "LongWrite starting...\n");
+  fprintf(destiny, "We'll write one line %d times at each %d miliseconds.\n",
+          times, sleep);
+  for (int i = 0; i < times; i++) {
+    fprintf(destiny, "%s\n", line);
+    fflush(destiny);
+    std::this_thread::sleep_for(std::chrono::milliseconds(sleep));
+  }
+  fclose(destiny);
 }
